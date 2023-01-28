8f49f3fff6e8e93cf8a6f1f268a856e32de11c35
Robingeorge12 <robingsa.btch@gmail.com>
2023-01-28 10:34:12 +0530

    bk

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express")
 const app = express()
 require('dotenv').config()
 const {connection} = require("./db.js")
-// const {signup} = require("../revision1backend/route/Signup.route")
-// const {login} = require("../revision1backend/route/Login.route")
-// const {bugs} = require("../revision1backend/route/Bugs.route")
+const {signup} = require("../revision1backend/route/Signup.route")
+const {login} = require("../revision1backend/route/Login.route")
+const {bugs} = require("../revision1backend/route/Bugs.route")
 var cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 7600 
 
-// app.use("/signup",signup)
-// app.use("/login",login)
-// app.use("/bugs",bugs)
+app.use("/signup",signup)
+app.use("/login",login)
+app.use("/bugs",bugs)
 
 app.get("/",(req,res)=>{
     res.send({"msg":"welcome to eavl"})
